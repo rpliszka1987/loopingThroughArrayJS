bc00ea9a05b4eaaadf7fe6d3ccee8d2e35bc3d10
Rob <rob@Robs-MacBook-Pro.local>
2020-02-18 13:22:38 -0500

    Added JS code to loop throught the array of play list and display to page

--- js/playlist.js
+++ js/playlist.js
@@ -4,10 +4,21 @@ var playList = [
   'Imagine',
   'Born to Run',
   'Louie Louie',
-  'Maybellene'
+  'Maybellene',
+  'Firestone'
 ];
 
 function print(message) {
   document.write(message);
 }
 
+function goThroughArray(list) {
+  var html = '<ol>';
+  for (var i = 0; i < list.length; i++) {
+    html += '<li>' + list[i] + '</li>';
+  }
+  html += '</ol>';
+  print(html);
+}
+
+goThroughArray(playList);
